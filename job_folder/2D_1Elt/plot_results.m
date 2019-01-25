a3703ec62199229ecf6d9b757ecb516072639435
Reuben Kraft <reuben.kraft@gmail.com>
2019-01-25 11:57:01 -0500

    updated 2D_1Elt

---
+++ job_folder/2D_1Elt/plot_results.m
@@ -0,0 +1,34 @@
+
+% Read Flagshyp file
+file=fopen('results.dat','r');
+%            step F11 F12 F21 F22 lnV sxx sxy syy
+formatSpec = '%d  %e  %e   %e  %e  %e  %e  %e  %e';
+sizeA = [9 inf ];
+A = fscanf(file,formatSpec,sizeA);
+fclose(file);
+
+% % Read abaqus file
+% file2=fopen('../abaqus3D/abaqus.rpt','r');
+% line_ex = fgetl(file2);
+% line_ex = fgetl(file2);
+% line_ex = fgetl(file2);
+% line_ex = fgetl(file2);
+% formatSpec = '%e %e %e';
+% sizeB = [3 inf ];
+% B = fscanf(file2,formatSpec,sizeB);
+% fclose(file2);
+
+plot(A(1,:),A(2,:),'-o','DisplayName','Flagshyp - F11','LineWidth',4)
+hold on
+plot(A(1,:),A(3,:),'-o','DisplayName','Flagshyp - F12','LineWidth',4)
+
+plot(A(1,:),A(4,:),'-s','MarkerSize',10,'MarkerEdgeColor','black','DisplayName','Flagshyp - F21','LineWidth',1)
+
+plot(A(1,:),A(5,:),'-o','DisplayName','Flagshyp - F22','LineWidth',4)
+
+legend('show')
+xlabel('Step')
+ylabel('Deformation Gradient')
+
+%plot(B(2,:),B(3,:),'*','DisplayName','Abaqus','LineWidth',4)
+
