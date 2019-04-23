2fa31d3b7e5c8d1888c415af9bb22983020e00bf
dis5403 <49924130+dis5403@users.noreply.github.com>
2019-04-23 19:15:01 -0400

    Add files via upload

---
+++ code/constitutive laws/stress/stress50.m
@@ -0,0 +1,17 @@
+%--------------------------------------------------------------------------
+% Evaluates the Cauchy stress tensor for material type 50.
+%--------------------------------------------------------------------------
+function Cauchy = stress50(kinematics,properties,cons,dimension)
+mu              = properties(2);  
+J               = kinematics.J;
+b               = kinematics.b;
+Ib              = kinematics.Ib;
+
+switch dimension
+    case 2
+         Ib     = Ib + 1;
+end
+
+Cauchy          = (mu*J^(-5/3))*(b - 1/3*Ib*cons.I); 
+%(6.55b) on page 171 and 312
+end
