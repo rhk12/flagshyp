4dfd40b197928b098c9dff16994b22bd9b27bc09
hzp54 <49964919+hzp54@users.noreply.github.com>
2019-04-24 20:06:56 -0400

    Completed stress file for Mooney-Rivlin

---
+++ code/constitutive laws/stress/stress2.m
@@ -0,0 +1,14 @@
+%--------------------------------------------------------------------------
+% Evaluates the Cauchy stress tensor (Mooney-Rivlin) for material type 2.
+%--------------------------------------------------------------------------
+function Cauchy = stress2(kinematics,properties,cons)
+mu           = properties(2); %Assume mu1 = mu2 = mu
+lambda       = properties(3); %Assume k = lambda
+j            = kinematics.J;
+B            = kinematics.b; %left cauchy green
+I_1          = trace(B);
+I_2          = 0.5*(I_1*I_1-trace(B*B));
+sigma_vol    = j^(-2/3)*(j-1)*lambda*cons.I;
+sigma_iso    = j^(-5/3)*(-1/3*(mu*I_1+2*mu*I_2)*cons.I+(mu+mu*I_1)*B+mu*B*B);
+Cauchy       = sigma_vol + sigma_iso;                      
+end
